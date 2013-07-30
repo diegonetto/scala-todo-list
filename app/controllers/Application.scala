9a27154a554820a4df9d82178ba087d5472f0025
Diego Netto <diegormnetto@gmail.com>
2013-07-30 15:44:52 -0500

    Implemented newTask Action

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -20,7 +20,15 @@ object Application extends Controller {
     Ok(views.html.index(Task.all(), taskForm))
   }
 
-  def newTask = TODO
+  def newTask = Action { implicit request =>
+    taskForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.index(Task.all(), errors)),
+      label => {
+        Task.create(label)
+        Redirect(routes.Application.tasks)
+      }
+    )
+  }
 
   def deleteTask(id: Long) = TODO
   
