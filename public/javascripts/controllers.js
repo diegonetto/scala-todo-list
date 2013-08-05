8d35b7d28167fc059c01c837267fcf2d3552d8c5
Diego Netto <diegormnetto@gmail.com>
2013-08-05 15:49:06 -0500

    Added temporary tasks and new styles.

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -3,5 +3,5 @@
 /* Controllers */
 
 function TodoListCtrl($scope) {
-  $scope.todos = ['hello', 'world'];
+  $scope.todos = ['hello', 'world!', 'simple', 'task', 'list', "This isn't interactive yet...", "... but it will be soon!", ';p'];
 }
